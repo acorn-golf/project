4791354bd6003a1c7736fc0087bc0fd7ad919e3e
tlakffja <tlakffja3@gmail.com>
2019-08-19 18:07:05 +0900

    lombok productDTO에 적용

--- src/com/squirrel/dto/ProductDTO.java
+++ src/com/squirrel/dto/ProductDTO.java
@@ -1,5 +1,8 @@
 package com.squirrel.dto;
 
+import lombok.Data;
+
+@Data
 public class ProductDTO {
 
 //	p_id	varchar2(5 byte)
@@ -51,85 +54,5 @@ public class ProductDTO {
 		this.cc_id = cc_id;
 		this.p_vcount = p_vcount;
 	}
-	public String getP_id() {
-		return p_id;
-	}
-	public void setP_id(String p_id) {
-		this.p_id = p_id;
-	}
-	public String getP_pdate() {
-		return p_pdate;
-	}
-	public void setP_pdate(String p_pdate) {
-		this.p_pdate = p_pdate;
-	}
-	public String getP_uploaddate() {
-		return p_uploaddate;
-	}
-	public void setP_uploaddate(String p_uploaddate) {
-		this.p_uploaddate = p_uploaddate;
-	}
-	public int getP_maxpeople() {
-		return p_maxpeople;
-	}
-	public void setP_maxpeople(int p_maxpeople) {
-		this.p_maxpeople = p_maxpeople;
-	}
-	public int getP_hole() {
-		return p_hole;
-	}
-	public void setP_hole(int p_hole) {
-		this.p_hole = p_hole;
-	}
-	public String getP_caddyyn() {
-		return p_caddyyn;
-	}
-	public void setP_caddyyn(String p_caddyyn) {
-		this.p_caddyyn = p_caddyyn;
-	}
-	public String getP_babyn() {
-		return p_babyn;
-	}
-	public void setP_babyn(String p_babyn) {
-		this.p_babyn = p_babyn;
-	}
-	public String getP_cartyn() {
-		return p_cartyn;
-	}
-	public void setP_cartyn(String p_cartyn) {
-		this.p_cartyn = p_cartyn;
-	}
-	public int getP_price() {
-		return p_price;
-	}
-	public void setP_price(int p_price) {
-		this.p_price = p_price;
-	}
-	public String getP_content() {
-		return p_content;
-	}
-	public void setP_content(String p_content) {
-		this.p_content = p_content;
-	}
-	public int getUser_no() {
-		return user_no;
-	}
-	public void setUser_no(int user_no) {
-		this.user_no = user_no;
-	}
-	public String getCc_id() {
-		return cc_id;
-	}
-	public void setCc_id(String cc_id) {
-		this.cc_id = cc_id;
-	}
-	public int getP_vcount() {
-		return p_vcount;
-	}
-	public void setP_vcount(int p_vcount) {
-		this.p_vcount = p_vcount;
-	}
-	
-	
 	
 }
